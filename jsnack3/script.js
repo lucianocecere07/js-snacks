a6a05b223a4ed3436bcd7f32d0ca3a227bf84224
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-14 16:56:46 +0100

    inserita somma

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,32 +1,21 @@
 
 //somma dei 10 numeri tramite un loop for (ERRORE!)
 let numero = 0;
-
-
+//let arrayNumeri =[];
+let somma =0;
 for(i=0; i<10; i++){
     let numero = parseInt(prompt("inserisci un numero intero"));
     console.log(numero);
-    let somma = numero[i] + numero[i];
-    somma = somma;
+    somma = somma + numero;
+    //arrayNumeri.push(numero);
 }
-console.log(somma);
+console.log("la somma dei numeri è: " + somma);
 
 
+/* SOMMA TRAMITE ARRAY
+for(i=0; i<arrayNumeri.length; i++){
 
+    somma = somma + arrayNumeri[i];
 
-/*
-let numero1 = parseInt(prompt("inserisci un numero intero"));
-let numero2 = parseInt(prompt("inserisci un secondo numero intero"));
-let numero3 = parseInt(prompt("inserisci un terzo numero intero"));
-let numero4 = parseInt(prompt("inserisci un quarto numero intero"));
-let numero5 = parseInt(prompt("inserisci un quinto numero intero"));
-let numero6 = parseInt(prompt("inserisci un sesto numero intero"));
-let numero7 = parseInt(prompt("inserisci un settimo numero intero"));
-let numero8 = parseInt(prompt("inserisci un ottavo numero intero"));
-let numero9 = parseInt(prompt("inserisci un nono numero intero"));
-let numero10 = parseInt(prompt("inserisci un decimo numero intero"));
-
-let somma = parseInt(numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10);
-
-console.log("la somma dei dieci numeri inseriti è: " + somma);
+}
 */
