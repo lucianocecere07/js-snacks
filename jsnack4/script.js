40599fea8c934b323252157aa210bd100ace6720
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-14 15:24:11 +0100

    jsnack4 vettore con nomi prompt utente e loop for

---
+++ jsnack4/script.js
@@ -0,0 +1,18 @@
+let arrayNomi = ["jay", "daisy", "myrtle", "jordan", "henry", "nick", "tom", "meyer", "george"];
+let nomeInvitatoFesta = false;
+
+let nomeUtente = prompt("Inserisci il tuo nome");
+let nomeUtenteMinuscolo = nomeUtente.toLowerCase();
+
+for (let i = 0; i < arrayNomi.length; i++){
+    let nomeInvitato = arrayNomi[i];
+    if (nomeInvitato == nomeUtenteMinuscolo){
+        nomeInvitatoFesta = true;
+        break;
+    }
+    if(!nomeInvitatoFesta){
+        console.log("Non sei stato invitato alla festa");
+    } else {
+        console.log("Puoi partecipare alla festa");
+    }
+}
