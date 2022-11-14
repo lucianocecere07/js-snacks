e354f0c8f8fd328cb0f552ec635224e4addf3b41
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-14 15:44:38 +0100

    loop for (inserire i 10 numeri)-somma errore

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,18 +1,20 @@
 
 //somma dei 10 numeri tramite un loop for (ERRORE!)
-/*
-let numero = parseInt(prompt("inserisci un numero intero"));
-let somma = 0;
-
+let numero = 0;
 for(i=0; i<10; i++){
-    somma = parseInt(numero);
+    let numero = parseInt(prompt("inserisci un numero intero"));
+    console.log(numero);
 }
 
-console.log("la somma dei 10 numeri inseriti è: " + somma);
-*/
+let somma = numero[i] + numero[i];
+console.log(somma);
 
 
 
+
+
+
+/*
 let numero1 = parseInt(prompt("inserisci un numero intero"));
 let numero2 = parseInt(prompt("inserisci un secondo numero intero"));
 let numero3 = parseInt(prompt("inserisci un terzo numero intero"));
@@ -27,3 +29,4 @@ let numero10 = parseInt(prompt("inserisci un decimo numero intero"));
 let somma = parseInt(numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10);
 
 console.log("la somma dei dieci numeri inseriti è: " + somma);
+*/
