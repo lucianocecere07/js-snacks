04f466dbfe56bb13641c33ad60be5a770c805d0d
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-14 14:56:16 +0100

    variabili e somma delle variabili

---
+++ jsnack3/script.js
@@ -0,0 +1,29 @@
+
+//somma dei 10 numeri tramite un loop for (ERRORE!)
+/*
+let numero = parseInt(prompt("inserisci un numero intero"));
+let somma = 0;
+
+for(i=0; i<10; i++){
+    somma = parseInt(numero);
+}
+
+console.log("la somma dei 10 numeri inseriti è: " + somma);
+*/
+
+
+
+let numero1 = parseInt(prompt("inserisci un numero intero"));
+let numero2 = parseInt(prompt("inserisci un secondo numero intero"));
+let numero3 = parseInt(prompt("inserisci un terzo numero intero"));
+let numero4 = parseInt(prompt("inserisci un quarto numero intero"));
+let numero5 = parseInt(prompt("inserisci un quinto numero intero"));
+let numero6 = parseInt(prompt("inserisci un sesto numero intero"));
+let numero7 = parseInt(prompt("inserisci un settimo numero intero"));
+let numero8 = parseInt(prompt("inserisci un ottavo numero intero"));
+let numero9 = parseInt(prompt("inserisci un nono numero intero"));
+let numero10 = parseInt(prompt("inserisci un decimo numero intero"));
+
+let somma = parseInt(numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10);
+
+console.log("la somma dei dieci numeri inseriti è: " + somma);
